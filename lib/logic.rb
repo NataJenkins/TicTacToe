742662441657d84a89d4d1ae65fb6c5e7bed16ff
Natalia <natajenkins@hotmail.com>
2020-11-09 09:10:35 -0600

    winning comp

--- lib/logic.rb
+++ lib/logic.rb
@@ -24,5 +24,13 @@ class Grid
     end
 
     def winning_positions
+        @win1 = @board[0] == @board[1] && @board[1] == @board[2]
+        @win2 = @board[3] == @board[4] && @board[4] == @board[5]
+        @win3 = @board[6] == @board[7] && @board[7] == @board[8]
+        @win4 = @board[0] == @board[3] && @board[3] == @board[6]
+        @win5 = @board[1] == @board[4] && @board[4] == @board[7]
+        @win6 = @board[2] == @board[5] && @board[5] == @board[8]
+        @win7 = @board[0] == @board[4] && @board[4] == @board[8]
+        @win8 = @board[2] == @board[4] && @board[4] == @board[6]
     end
 end
