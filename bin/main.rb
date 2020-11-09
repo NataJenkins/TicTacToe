d4b5ccaa113f87b92ea04a2d60aec5d3f8031552
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-11-09 10:17:00 -0700

    Fix the game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'artii'
 require 'colorize'
-require './lib/logic.rb'
+require './lib/logic'
 
 title = Artii::Base.new
 
@@ -17,38 +17,37 @@ puts 'Enter your name Player 2: '.colorize(:green)
 player2 = gets.chomp
 player2 = Player.new(player2, 'O')
 
-grid = Grid.new()
+grid = Grid.new
 
-until grid.winner 
+until grid.winner
 
-  puts grid.board
+  puts grid.board_display
 
   puts "#{player1.name} pick a number from 1 to 9: ".colorize(:green)
- 
-  puts grid.board
+
+  puts grid.board_display
   while true
     input1 = gets.chomp.to_i
-    if grid.check_valid?(input1-1)
-      grid.update_board(player1, input1-1) 
+    if grid.check_valid?(input1 - 1)
+      grid.update_board(player1, input1 - 1)
       break
     else
       puts "#{input1} is already occupied"
+      puts "#{player1.name} pick a number from 1 to 9: ".colorize(:green)
     end
   end
 
-
   puts "#{player1.name} has selected the #{input1} position".colorize(:green)
-  puts grid.board
+  puts grid.board_display
 
-  grid.winning_positions(player1,player2)
-  break if grid.winner?
+  break if grid.winner?(player1, player2)
 
   puts "#{player2.name} pick a number from 1 to 9: ".colorize(:green)
-  puts grid.board
+  puts grid.board_display
   while true
     input2 = gets.chomp.to_i
-    if grid.check_valid?(input2-1)
-      grid.update_board(player2, input2-1) 
+    if grid.check_valid?(input2 - 1)
+      grid.update_board(player2, input2 - 1)
       break
     else
       puts "#{input2} is already occupied"
@@ -56,10 +55,9 @@ until grid.winner
     end
   end
   puts "#{player2.name} has selected the #{input2} position".colorize(:green)
-  puts grid.board
-  
-  grid.winning_positions(player1,player2)
-  break if grid.winner?
+  puts grid.board_display
+
+  break if grid.winner?(player1, player2)
 end
 
 puts "🎉 #{grid.winner.name} wins the game 🎉".colorize(:green) unless grid.winner == 'draw'
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,64 +1,61 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+
 require 'colorize'
 
 class Player
-    attr_reader :name
-    attr_reader :token
-    def initialize(name, token)
-        @name = name
-        @token = token
-    end
+  attr_reader :name, :token
 
+  def initialize(name, token)
+    @name = name
+    @token = token
+  end
 end
 
 class Grid
-    attr_reader :board
-    attr_reader :winner
+  attr_reader :board, :winner
 
-    def initialize()
-        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        @winner = nil
+  def initialize()
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @winner = nil
+  end
+
+  def board_display
+    row1 = "| #{@board[0]} | #{@board[1]} | #{@board[2]} |\n".colorize(:light_blue)
+    separator = "--------------\n".colorize(:light_blue)
+    row2 = "| #{@board[3]} | #{@board[4]} | #{@board[5]} |\n".colorize(:light_blue)
+    row3 = "| #{@board[6]} | #{@board[7]} | #{@board[8]} |\n".colorize(:light_blue)
+    row1 + separator + row2 + separator + row3
+  end
+
+  def update_board(player, position)
+    @board[position] = player.token
+  end
+
+  def winner?(player1, player2)
+    winning_conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    x_position = @board.each_with_index
+      .select { |val, _index| val == player1.token }
+      .map { |row| row[1] }
+    y_position = @board.each_with_index
+      .select { |val, _index| val == player2.token }
+      .map { |row| row[1] }
+    if winning_conditions.include?(x_position)
+      @winner ||= player1
+    elsif winning_conditions.include?(y_position)
+      @winner ||= player1
+    elsif @board.all?(String)
+      @winner = 'draw'
     end
+    @winner
+  end
 
-    def board
-        row1 = "| #{@board[0]} | #{@board[1]} | #{@board[2]} |\n".colorize(:light_blue)
-        separator = "--------------\n".colorize(:light_blue)
-        row2 = "| #{@board[3]} | #{@board[4]} | #{@board[5]} |\n".colorize(:light_blue)
-        row3 = "| #{@board[6]} | #{@board[7]} | #{@board[8]} |\n".colorize(:light_blue)
-        return row1  + separator + row2 + separator + row3
-    end
+  def check_valid?(position)
+    return false if @board[position] == 'X' || @board[position] == 'O'
 
-    def update_board(player, position)
-        @board[position] = player.token 
-    end
-
-    def winning_positions(player1, player2)
-        winning_conditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
-        x_position = @board.each_with_index
-            .select { |val, index| val == player1.token }
-            .map { |row| row[1] }
-        y_position = @board.each_with_index
-            .select { |val, index| val == player2.token }
-            .map { |row| row[1] }
-        if winning_conditions.include?(x_position)
-            @winner ||= player1
-        elsif winning_conditions.include?(y_position)
-            @winner ||= player1
-        elsif @board.all?(String) 
-            @winner = "draw"
-        end 
-    end
-
-    def check_valid?(position)
-        return false if @board[position] == 'X' || @board[position] == 'O'
-        true
-    end
-
-
-    def winner?
-        if @board.all?(String) 
-            return "draw"
-        else
-            return @winner
-        end
-    end
+    true
+  end
 end
+
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
