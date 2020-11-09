9cab0030c6baa5f90fdda6c0994aff4f0a2c9af7
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-11-09 09:20:42 -0700

    Add game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'artii'
 require 'colorize'
+require './lib/logic.rb'
 
 title = Artii::Base.new
 
@@ -10,61 +11,45 @@ puts "\n"
 
 puts 'Enter your name Player 1: '.colorize(:green)
 player1 = gets.chomp
+player1 = Player.new(player1, 'X')
 
 puts 'Enter your name Player 2: '.colorize(:green)
 player2 = gets.chomp
+player2 = Player.new(player2, 'O')
 
-board = %w[1 2 3 4 5 6 7 8 9]
+grid = Grid.new()
 
-game_on = true
-# While game end
-winner = nil
-while game_on
+until grid.winner 
 
-  def return_board(board)
-    puts "| #{board[0]} | #{board[1]} | #{board[2]} |".colorize(:light_blue)
-    puts '--------------'.colorize(:light_blue)
-    puts "| #{board[3]} | #{board[4]} | #{board[5]} |".colorize(:light_blue)
-    puts '--------------'.colorize(:light_blue)
-    puts "| #{board[6]} | #{board[7]} | #{board[8]} |".colorize(:light_blue)
-    puts "\n"
+  puts grid.board
+
+  puts "#{player1.name} pick a number from 1 to 9: ".colorize(:green)
+ 
+  puts grid.board
+  input1 = gets.chomp.to_i
+  until grid.check_valid?(input1)
+    puts "#{input1} is already occupied"
   end
+  grid.update_board(player1, input1-1) 
 
-  puts "#{player1} pick a number from 1 to 9: ".colorize(:green)
-  # show the board without selection
-  return_board(board)
-  input1 = gets.chomp
-  input1 = input1.to_i
+  puts "#{player1.name} has selected the #{input1} position".colorize(:green)
+  puts grid.board
 
-  board[input1.to_i - 1] = 'X'
-  puts "#{player1} has selected the #{input1} position".colorize(:green)
-  return_board(board)
-  # show the board with selection
-
-  puts "#{player2} pick a number from 1 to 9: ".colorize(:green)
-  # show the board without selection
-  return_board(board)
-  input2 = gets.chomp
-  if board[input2.to_i - 1] == 'X'
-    print 'Sorry this move is not valid, '
-    p 'please select another place'
-  else
-    board[input2.to_i - 1] = 'O'
-    puts "#{player2} has selected the #{input2} position".colorize(:green)
-    return_board(board)
+  puts "#{player2.name} pick a number from 1 to 9: ".colorize(:green)
+  puts grid.board
+  input2 = gets.chomp.to_i
+  until grid.check_valid?(input2)
+    puts "#{input2} is already occupied"
   end
+  grid.update_board(player2, input2-1)
+  puts "#{player2.name} has selected the #{input2} position".colorize(:green)
+  puts grid.board
+  
+  grid.winning_positions(player1,player2)
+  break if grid.winner?
 
-  if board.count('X') == 3
-    winner ||= player1
-  elsif board.count('O') == 3
-    winner ||= player2
-  elsif board.count('X') == 4 && board.count('O') == 4
-    winner ||= 'draw'
-  end
-
-  game_on = false if winner
 
 end
 
-puts "🎉 #{winner} wins the game 🎉".colorize(:green) unless winner == 'draw'
-puts 'Game is draw' if winner == 'draw'
+puts "🎉 #{grid.winner.name} wins the game 🎉".colorize(:green) unless grid.winner == 'draw'
+puts 'Game is draw' if grid.winner == 'draw'
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Player
     attr_reader :name
     attr_reader :token
@@ -9,32 +11,54 @@ class Player
 end
 
 class Grid
-    @board = %w[1 2 3 4 5 6 7 8 9]
+    attr_reader :board
+    attr_reader :winner
+
+    def initialize()
+        @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        @winner = nil
+    end
+
     def board
-        puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |".colorize(:light_blue)
-        puts '--------------'.colorize(:light_blue)
-        puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |".colorize(:light_blue)
-        puts '--------------'.colorize(:light_blue)
-        puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |".colorize(:light_blue)
-        puts "\n"
+        row1 = "| #{@board[0]} | #{@board[1]} | #{@board[2]} |\n".colorize(:light_blue)
+        separator = "--------------\n".colorize(:light_blue)
+        row2 = "| #{@board[3]} | #{@board[4]} | #{@board[5]} |\n".colorize(:light_blue)
+        row3 = "| #{@board[6]} | #{@board[7]} | #{@board[8]} |\n".colorize(:light_blue)
+        return row1  + separator + row2 + separator + row3
     end
 
-    def update_board(user_input, position)
-        @board[position] = user_input 
+    def update_board(player, position)
+        @board[position] = player.token 
     end
 
-    def winning_positions
-        @win1 = @board[0] == @board[1] && @board[1] == @board[2]
-        @win2 = @board[3] == @board[4] && @board[4] == @board[5]
-        @win3 = @board[6] == @board[7] && @board[7] == @board[8]
-        @win4 = @board[0] == @board[3] && @board[3] == @board[6]
-        @win5 = @board[1] == @board[4] && @board[4] == @board[7]
-        @win6 = @board[2] == @board[5] && @board[5] == @board[8]
-        @win7 = @board[0] == @board[4] && @board[4] == @board[8]
-        @win8 = @board[2] == @board[4] && @board[4] == @board[6]
+    def winning_positions(player1, player2)
+        winning_conditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,7],[0,3,6],[1,4,7],[2,5,8]]
+        x_position = @board.each_with_index
+            .select { |val, index| val == player1.token }
+            .map { |row| row[1] }
+        y_position = @board.each_with_index
+            .select { |val, index| val == player2.token }
+            .map { |row| row[1] }
+        if winning_conditions.include?(x_position)
+            @winner ||= player1
+        elsif winning_conditions.include?(y_position)
+            @winner ||= player1
+        elsif @board.all?(String) 
+            @winner = "draw"
+        end 
     end
 
-    def draw
-        @board.all?(String)
+    def check_valid?(position)
+        return true if @board[position].include?()
+        true
+    end
+
+
+    def winner?
+        if @board.all?(String) && @winner = nil
+            return "draw"
+        else
+            return @winner
+        end
     end
 end
