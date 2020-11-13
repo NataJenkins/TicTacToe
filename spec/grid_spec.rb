66ab7d166de8a10398ac5c90905268a943f09875
Natalia <natajenkins@hotmail.com>
2020-11-13 14:51:33 -0600

    added test cases

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -12,5 +12,22 @@ describe Grid do
       input = grid.check_valid?(10)
       expect(input).to eq(false)
     end
+    it 'Check input is not negative' do
+        grid = Grid.new
+        input = grid.check_valid?(-1)
+        expect(input).to eq(false)
+    end
   end
+  describe "initialize" do
+    it 'check if there is an istance of Grid' do
+    grid = Grid.new
+    expect(grid).to an_instance_of(Grid)
+  end
+  end
+#   describe "#marks" do
+#     it "check marks are in the right position"do
+#     grid = Grid.new
+#     expect(grid.assigning_marks(1)).to 
+#     end
+#   end
 end
