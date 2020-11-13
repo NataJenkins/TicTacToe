5fdc7a660c76f88c2623555b396d8c614129a47a
Natalia <natajenkins@hotmail.com>
2020-11-13 13:49:03 -0600

    Linter fix

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,25 +1,16 @@
 require './lib/grid'
 
 describe Grid do
-    describe "#check_valid?" do
-        it "Check input is between 1 and 9" do
-            grid = Grid.new
-            input = grid.check_valid?(1)
-            expect(input).to be_within(1)
-        end
-        it "Check input is between 1 and 9" do
-            grid = Grid.new
-            input = grid.check_valid?(10)
-            expect(input).to eq (false)
-        end
+  describe '#check_valid?' do
+    it 'Check input is between 1 and 9' do
+      grid = Grid.new
+      input = grid.check_valid?(1)
+      expect(input).to be_within(1)
     end
-end
-describe grid do 
-    describe "#winner?" do
-        it "check winning combination"
-        grid = Grid.new
-        win_loss=grid.winner?("ab", "ba")
-        expect(win_loss).to eq(true)
+    it 'Check input is between 1 and 9' do
+      grid = Grid.new
+      input = grid.check_valid?(10)
+      expect(input).to eq(false)
     end
-
+  end
 end
