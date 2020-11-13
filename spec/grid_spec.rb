4175b16f5a22013b363d8b55b93304d736124853
Natalia <natajenkins@hotmail.com>
2020-11-13 11:37:59 -0600

    Valid grid position test

---
+++ spec/grid_spec.rb
@@ -0,0 +1,16 @@
+require './lib/grid'
+
+describe Grid do
+    describe "#check_valid?" do
+        it "Check input is between 1 and 9" do
+            grid = Grid.new
+            input = grid.check_valid?(1)
+            expect(input).to eq (true)
+        end
+        it "Check input is between 1 and 9" do
+            grid = Grid.new
+            input = grid.check_valid?(10)
+            expect(input).to eq (false)
+        end
+    end
+end
