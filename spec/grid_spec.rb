64b2bb75c11685d1cfdce0910ebfe167c5ee27e1
Natalia <natajenkins@hotmail.com>
2020-11-13 14:31:24 -0600

    fix grid test error

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -5,7 +5,7 @@ describe Grid do
     it 'Check input is between 1 and 9' do
       grid = Grid.new
       input = grid.check_valid?(1)
-      expect(input).to be_within(1)
+      expect(input).to eq(true)
     end
     it 'Check input is between 1 and 9' do
       grid = Grid.new
