e8ac295366bb5424b4adced682dc6c1a1f844493
Natalia <natajenkins@hotmail.com>
2020-11-16 12:47:13 -0600

    grid tests added

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -36,9 +36,21 @@ describe Grid do
       grid = Grid.new
       expect { grid.winner?(player1, player2, player3) }.to raise_error(ArgumentError)
     end
-    it 'raises an argument error ' do
+    it '#raises an argument error ' do
       grid = Grid.new
       expect { grid.winner?(player1, player2) }
     end
   end
+   describe'#update_board' do
+     it 'board is getting the player token'do
+     grid = Grid.new
+     expect {grid.update_position("one", 5).to eq("X")}
+   end
+   end
+  describe '#player_position'do
+  it 'returns an instance of an array' do
+  grid = Grid.new
+  expect {grid.player_position(player1).to an_instance_of(Array)}
+  end
+end
 end
